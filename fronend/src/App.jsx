0479f5f6ed5dc2f97b6313bec6f94d151a1c8f28
Hernan M <her.laplata@gmail.com>
2026-02-26 06:03:57 -0300

    Se agregaron estilos

--- fronend/src/App.jsx
+++ fronend/src/App.jsx
@@ -37,16 +37,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Mensaje' onChange={(e) => setMessage(e.target.value)}></input>
+      <div className='h-screen bg-zinc-800 text-white flex items-center justify-center'>
+        <form onSubmit={handleSubmit} className='bg-zinc-900 p-10'>
+          <h1 className='text-2xl font-bold my-2'>Chat utn</h1>
+          <input type="text" placeholder='Mensaje' className='border-2 border-zinc-500 p-2 w-full text-white'
+            onChange={(e) => setMessage(e.target.value)}></input>
           <button>Enviar</button>
+          <ul>
+            {messages.map((message, i) => (
+              <li key={i}
+                className={`{my-2 p-2 table text-sm rounded-md ${message.from == 'Me' ? `bg-sky-700` : `bg-black ml-auto`}`}>{message.from}:{message.body}</li>
+            ))}
+          </ul>
         </form>
-        <ul>
-          {messages.map((message, i) => (
-            <li key={i}>{message.from}:{message.body}</li>
-          ))}
-        </ul>
       </div>
     </>
   )
